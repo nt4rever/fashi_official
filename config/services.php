f3e872d2b82fade0fefdd7112adf4a9c57c1149d
nt4rever <levantanald@gmail.com>
2021-09-02 19:36:05 +0700

    add call back google

--- config/services.php
+++ config/services.php
@@ -30,14 +30,14 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
     'facebook' => [
-        'client_id' => '230489531811041',  //client face của bạn
-        'client_secret' => '00ca29d9956305d3660211408b2c1d40',  //client app service face của bạn
-        'redirect' => 'https://fashi.com/GIT_fix/public/login-customer/callback' //callback trả về
+        'client_id' => env('FACEBOOK_APP_ID'),  //client face của bạn
+        'client_secret' => env('FACEBOOK_APP_SECRET'),  //client app service face của bạn
+        'redirect' => env('FACEBOOK_REDIRECT') //callback trả về
     ],
     'google' => [
-        'client_id' => '313861602513-0ctqvactttgkb0ogb1nd5i6ur2v0380a.apps.googleusercontent.com',
-        'client_secret' => 'hWC-PcU9MxkQwFUY5dG_xLg8',
-        'redirect' => 'https://fashi.com/GIT_fix/public/login-customer/google/callback'
+        'client_id' => env('GOOGLE_ID'),
+        'client_secret' => env('GOOGLE_SECRET'),
+        'redirect' => env('GOOGLE_URL')
     ],
 
 
